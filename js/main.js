6b96b515ca190237ff20b6d774b1f951d3cd14bb
Loic <lginoux@gmail.com>
2012-11-26 15:27:42 +0000

    contiue API doc

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+$(function() {
+	// toggle all variables
+	$(".toggle_variables").click(function(){
+		$(".variables h4").click();
+	});
 
+	// toggle all methods
+	$(".toggle_methods").click(function(){
+		$(".methods h4").click();
+	});
+
+	// api side bar
+    $('.api .sidebar-nav').affix({
+		offset:{
+			top:0,
+			bottom: 0
+		}
+    });
+
+
+});
