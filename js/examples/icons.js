840a9f37313786ae545ef8b042da799a927e3d4a
Loic <lginoux@gmail.com>
2012-11-27 14:46:53 +0000

    more API documentation

---
+++ js/examples/icons.js
@@ -0,0 +1,46 @@
+(function() {
+
+var tree = Vtree.create({
+	container: jQuery("#treeContainerIcons"),
+	dataSource: {
+		tree: {
+			id: "iconsTree",
+			nodes: [{
+				id: "master",
+				title: "master node",
+				customClass: "title",
+				iconClass:"default",
+				hasChildren: true,
+				nodes: [{
+					id: "parent_1",
+					title: "parent 1",
+					iconPath: {
+						open: "/vtree/img/icon_folder_open_16.png",
+						close: "/vtree/img/icon_folder_closed_16.png"
+					},
+					hasChildren: true,
+					nodes: [{
+						id: "child_1",
+						title: "child 1",
+						iconPath: "/vtree/img/file.png",
+						hasChildren: false
+					}]
+				},{
+					id: "parent_2",
+					title: "parent 2",
+					iconClass: "customFolder",
+					hasChildren: true,
+					nodes: [{
+						id: "child_2",
+						title: "child 2",
+						iconClass: "customFile",
+						hasChildren: false
+					}]
+				}]
+			}]
+		}
+	},
+	initially_open: ["parent_1", "parent_2"] // all parents will be open too
+});
+
+})();
